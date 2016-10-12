d65d0d12f4a6fa13f9df442df0434cface0ab591
Joa A <Joa A>
2016-10-12 10:39:39 +0200

    correction bug du numero INT-> STRING & adatation au mode assynchrone

---
+++ src/utils/Tools.java
@@ -0,0 +1,29 @@
+package utils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Tools{
+
+	/**
+	 * renvoie un message
+	 * @param msg
+	 * @return  
+	 * @throws JSONException 
+	 */
+	public static JSONObject serviceMessage(Object msg) throws JSONException{
+		return new JSONObject().put("message",msg);}
+
+	/**
+	 * renvoie un message d'erreur et le code d'erreur correspondant
+	 * @param msg
+	 * @param errorCode
+	 * @return 
+	 */
+
+	public static JSONObject serviceRefused(Object msg, int errorCode)throws JSONException{
+		JSONObject json=new JSONObject();
+		json.put("erreur",msg);
+		json.put("erreurcode",errorCode);
+		return json;}
+}
