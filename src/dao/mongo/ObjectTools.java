2861e3f436a6848edf43d71faa93fed13911361c
digitex3d <digitex3d@gmail.com>
2016-11-03 12:27:21 +0100

    Amélioration connexion BD

--- src/dao/mongo/ObjectTools.java
+++ src/dao/mongo/ObjectTools.java
@@ -6,18 +6,15 @@ import java.util.Date;
 
 import org.json.JSONObject;
 
-import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
-import com.mongodb.Mongo;
 import com.mongodb.MongoException;
-
 
 /**
  * Contient les methodes pour gérer les objets dans la base de données.
  */
 public class ObjectTools {
-public static final String MDB_COLLECTION = "objets";
+	public static final String MDB_OBJECTS_COLLECTION = "objets";
 	
 	/**
 	 * Ajoute un objet à la base mongo
@@ -27,14 +24,8 @@ public static final String MDB_COLLECTION = "objets";
 	 * @throws MongoException
 	 */
 	public static void addObject(JSONObject object) throws UnknownHostException, MongoException{
-		// COnnexion mongo
-		Mongo m = MongoConnection.getConnection();
-		
-		// Base de données
-		DB db = m.getDB(MongoConnection.MONGO_DB_NAME);
-		
 		// Instance de la collection
-		DBCollection collection = db.getCollection(MDB_COLLECTION);
+		DBCollection collection = MongoConnection.getCollection(MDB_OBJECTS_COLLECTION);
 
 		// Parsing de l'objet
 		DBObject dbObj = (DBObject) com.mongodb.util.JSON.parse(object.toString());
@@ -46,7 +37,7 @@ public static final String MDB_COLLECTION = "objets";
 		
 		// Ajout dans la base de données
 		collection.insert( dbObj ).toString();
-		m.close();
+
 		
 	}
 
