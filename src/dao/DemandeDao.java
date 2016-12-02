4d54c9dc3773491504837f84bb2a0416355c8c6a
A joan <tutanck@gmail.com>
2016-12-02 12:33:36 +0100

    hkjl

--- src/dao/DemandeDao.java
+++ src/dao/DemandeDao.java
@@ -10,20 +10,25 @@ import kasudb.KasuDB;
 
 public class DemandeDao {
 
+	private static final String table_name = "demande_emprunt";
 	
-	public static void  AjouterDemande(String idSend,String idRecep,String idObject)throws SQLException
+	public static void  AjouterDemande(int idSend,String idObject)throws SQLException
 	{
-		String sqlQuery = "INSERT INTO DEMANDE values ('"+idSend+"','"+idRecep+"','"+idObject+"');";
+		String sqlQuery = "INSERT INTO "+table_name +" values ('"+idSend+"','"+idObject+"',null,null,NOW());";
 		Connection c = KasuDB.SQLConnection();
 		Statement s = c.createStatement();
 		s.executeUpdate(sqlQuery);
 		s.close();
 		c.close();	
 	}
+	
+	public static void main(String[] args) throws SQLException {
+		AjouterDemande(1,"idObject");
+	}
 	public static ArrayList<Integer> myDemande(String user)throws SQLException
 	{
 		ArrayList<Integer> myDemandes = new ArrayList<Integer>();
-		String sql = "SELECT idSend FROM DEMANDE WHERE (idRecep='"+user+"');";
+		String sql = "SELECT idSend FROM "+table_name +" WHERE (idRecep='"+user+"');";
 		Connection connection = KasuDB.SQLConnection();
 		Statement statement = connection.createStatement();
 		ResultSet resultSet = statement.executeQuery(sql);
