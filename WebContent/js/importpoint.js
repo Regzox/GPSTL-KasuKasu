f82a371f8e3be9ea33026ec339216b2d79be3066
Linayahi <yahi.liina@gmail.com>
2017-01-18 16:33:24 +0100

    lieux d'échange

--- WebContent/js/importpoint.js
+++ WebContent/js/importpoint.js
@@ -1,17 +1,20 @@
 var map;
 bool=0;
 if(document.cookie.search("lang=en")!=-1)
-		bool=1;
-	else
-		if(document.cookie.search("lang=fr")!=-1)
-			bool=0;
+	bool=1;
+else
+	if(document.cookie.search("lang=fr")!=-1)
+		bool=0;
 var id; 
 var name; 
 var radius;
 
 var json1; 
 var json2;
-/* idée : regrouper json1 et 2 en une seule fonction d'affichage */
+var reponse1 = false;
+var reponse2 = false;
+var table_feature1 = new Array();
+var table_feature2 = new Array(); 
 
 OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
 	defaultHandlerOptions : {
@@ -37,13 +40,13 @@ OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
 	trigger : function(e) {
 		var toProjection = new OpenLayers.Projection("EPSG:4326");
 		var lonlat = map.getLonLatFromViewPortPx(e.xy);
-		
+
 		abonnement(lonlat.lat.toFixed(5),lonlat.lon.toFixed(5));
-		
+
 	},
-	
+
 	trigger2 : function(e) {
-		
+
 	}
 });
 
@@ -59,11 +62,21 @@ function init()
 
 	var zoom = 14;
 	map.setCenter(lonLat, zoom);
-	//Afficher les points d'échange des amis de l'utilisateur auxquels il peut s'abonner
-	//Afficher les points d'échange de l'utilisateur
-	userPoints();
 
+	//Récupérer les points d'échange des amis de l'utilisateur auxquels il peut s'abonner
 	userFriendsPoints();
+	reponse1=true;
+
+	//Récupérer les points d'échange de l'utilisateur
+	userPoints();
+	reponse2=true;
+
+	//Afficher les lieux une fois que les deux callback sont à true
+	if (reponse1==true && reponse2==true )
+	{
+		affiche();
+	}
+
 
 	var click = new OpenLayers.Control.Click();
 	map.addControl(click);
@@ -75,38 +88,38 @@ function init()
 /************************************* S'abonner à un point d'échange ************************************/
 function abonnement(lat,lon)
 {
-    $("#myModal").modal({                    
-	      "backdrop"  : "static",
-	      "keyboard"  : true,
-	      "show"      : true                     
-	    });
-    
-    
-    $("#myModal").on('hidden.bs.modal', function () {
-	    $(this).removeData('modal');
-         window.location.href = importpoint_jsp;
-  });  
+	$("#myModal").modal({                    
+		"backdrop"  : "static",
+		"keyboard"  : true,
+		"show"      : true                     
+	});
 
-   
+
+	$("#myModal").on('hidden.bs.modal', function () {
+		$(this).removeData('modal');
+		window.location.href = importpoint_jsp;
+	});  
+
+
 	$('#save').on(
 			'click',
 			function(evt)
 			{
-			    nom_abo = $("#myModal").find("#nom_input").val();
-			    radius_abo = $("#myModal").find("#radius_input").val();
-			    
-			    if (nom_abo.length!=0 && radius_abo.length!=0)
-			    	{
-				       addPoint(nom_abo,radius_abo,lat,lon); 
-			    	}
-			    
-			    else $("#myModal").hide(); 
+				nom_abo = $("#myModal").find("#nom_input").val();
+				radius_abo = $("#myModal").find("#radius_input").val();
+
+				if (nom_abo.length!=0 && radius_abo.length!=0)
+				{
+					addPoint(nom_abo,radius_abo,lat,lon); 
+				}
+
+				else $("#myModal").hide(); 
 
 			}
-			);
-	
+	);
 
- }
+
+}
 
 function addPoint(nom_abo,radius_abo,lat,lon)
 {
@@ -123,33 +136,34 @@ function addPoint(nom_abo,radius_abo,lat,lon)
 		success : function (data)
 		{
 
-	        if (data.message=="1")
-	        	{
-	    	       $("#myModal").hide(); 
-	    	       
-	    	       $("#myModal2").modal({                    
-	     		      "backdrop"  : "static",
-	     		      "keyboard"  : true,
-	     		      "show"      : true                     
-	     		    });
-	     	       
-	     	       $("#myModal2").on('hidden.bs.modal', function () {
-	     	           window.location.href = importpoint_jsp;
-	     	       });
-   	    
-	        	}
+			if (data.message=="1")
+			{
+				$("#myModal").hide(); 
+
+				$("#myModal2").modal({                    
+					"backdrop"  : "static",
+					"keyboard"  : true,
+					"show"      : true                     
+				});
+
+				$("#myModal2").on('hidden.bs.modal', function () {
+					window.location.href = importpoint_jsp;
+				});
+
+			}
 		},		error : function(xhr,status,errorthrown){
 			console.log(JSON.stringify(xhr + " " + status + " " + errorthrown));
 		}
 	});
-	
+
 }
 
-/********************************** Afficher les points des amis de l'utilisateur ************************************/
+/********************************** Récupérer les points des amis de l'utilisateur ************************************/
 
 
 function userFriendsPoints(){
 	$.ajax({
+		async : false,
 		type : "GET",
 		url : "PointsUserFriendList",
 		data : "",
@@ -165,70 +179,15 @@ function userFriendsPoints(){
 
 function traiteReponse2(json) 
 {
-    var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
 
-
-	for (var i=0; i< json.expts.length; i++)
-		{
-
-	      var lon = json.expts[i].lon;
-	      var lat = json.expts[i].lat;
-	      var feature;
-	       if(bool==0)
-	        feature = new OpenLayers.Feature.Vector
-	        (
-
-	                new OpenLayers.Geometry.Point( lon, lat ),
-	                {description: "Mes amis sur ce lieu: "+json.expts[i].name} ,
-	                {externalGraphic: '/KasuKasu/data/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
-	         ); 
-	       if(bool==1)
-		        feature = new OpenLayers.Feature.Vector
-		        (
-
-		                new OpenLayers.Geometry.Point( lon, lat ),
-		                {description: "My friends in this place : "+json.expts[i].name} ,
-		                {externalGraphic: '/KasuKasu/data/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
-		         ); 
-	        vectorLayer.addFeatures(feature);
-
-		}
-	
-    map.addLayer(vectorLayer);
-    
-    var controls = {
-    	      selector: new OpenLayers.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
-    	    };
-
-    	    function createPopup(feature) {
-    	    	
-    	    	var lonLat = feature.geometry.getBounds().getCenterLonLat();
-
-    	         feature.popup = new OpenLayers.Popup.FramedCloud("pop",
-    	          feature.geometry.getBounds().getCenterLonLat(),
-    	          null, 	          
-    	          '<div class="markerContent">'+feature.attributes.description+'</div>'+'<br></br>'+'<button onclick="abonnement(\'' + lonLat.lat + '\',\'' + lonLat.lon + '\')">S\'abonner</button>',
-    	          null,
-    	          true,
-    	          function() { controls['selector'].unselectAll(); }
-    	      );
-    	      //feature.popup.closeOnMove = true;
-    	      map.addPopup(feature.popup);
-    	    }
-
-    	    function destroyPopup(feature) {
-    	      feature.popup.destroy();
-    	      feature.popup = null;
-    	    }
-    	    
-    	    map.addControl(controls['selector']);
-    	    controls['selector'].activate();
+	json1=JSON.parse(JSON.stringify(json));
 
 }
 
-/********************************** Afficher les points de l'utilisateur ************************************/
+/********************************** Récupérer les points de l'utilisateur ************************************/
 function userPoints(){
 	$.ajax({
+		async : false,
 		type : "GET",
 		url : PointsUserList,
 		data : "",
@@ -242,78 +201,8 @@ function userPoints(){
 
 function traiteReponse(json) 
 {
-    var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
 
-
-	for (var i=0; i< json.expts.length; i++)
-		{
-
-	      var lon = json.expts[i].lon;
-	      var lat = json.expts[i].lat;
-	      id = json.expts[i].id;
-	      var feature;
-	       if(bool==0)
-	        feature = new OpenLayers.Feature.Vector
-	        (
-
-	                new OpenLayers.Geometry.Point( lon, lat ),
-	                {description: "Lieu : "+json.expts[i].name+", "+json.expts[i].radius+"m"},
-	                {externalGraphic: '/KasuKasu/data/marker-red.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
-	         ); 
-	       if(bool==1)
-		        feature = new OpenLayers.Feature.Vector
-		        (
-
-		                new OpenLayers.Geometry.Point( lon, lat ),
-		                {description: "Place : "+json.expts[i].name} ,
-		                {externalGraphic: '/KasuKasu/data/marker-red.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
-		         ); 
-	        vectorLayer.addFeatures(feature);
-
-		}
-	
-    map.addLayer(vectorLayer);
-    
-    var controls = {
-    	      selector: new OpenLayers.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
-    	    };
-
-    	    function createPopup(feature) 
-    	    {
-
-    	    	var lonLat = feature.geometry.getBounds().getCenterLonLat();
-//    			document.getElementById('lat').value=lonLat.lat;
-//    			document.getElementById('lon').value=lonLat.lon;
-    			
-    			id="";
-    			name="";
-    			radius="";
-    			
-    			getMarkerDetail(lonLat.lat,lonLat.lon);
-//    			console.log(id);
-//    			console.log(name);
-//    			console.log(radius);
-
-    	          feature.popup = new OpenLayers.Popup.FramedCloud("popup",
-    	          feature.geometry.getBounds().getCenterLonLat(),
-    	          null, 	          
-    	          '<div class="markerContent">'+feature.attributes.description+'</div>'+'<br></br>'+'<button onclick="Modifier(\'' + id + '\',\'' + name + '\' ,\'' + radius+ '\')"> Modifier</button>'+'<button onclick="Delete(\''+id+'\');">Supprimer</button>',
-    	          null,
-    	          true,
-    	          function() { controls['selector'].unselectAll(); }
-    	      );
-    	          
-    	      //feature.popup.closeOnMove = true;
-    	      map.addPopup(feature.popup);
-    	    }
-
-    	    function destroyPopup(feature) {
-    	      feature.popup.destroy();
-    	      feature.popup = null;
-    	    }
-    	    
-    	    map.addControl(controls['selector']);
-    	    controls['selector'].activate();
+	json2=JSON.parse(JSON.stringify(json));
 
 }
 
@@ -329,18 +218,18 @@ function Delete(id)
 		success : function (data)
 		{
 
-	        if (data.message=="1")
-	        	{
-	    	       $("#myModal3").modal({                    
-	    		      "backdrop"  : "static",
-	    		      "keyboard"  : true,
-	    		      "show"      : true                     
-	    		    });
-	    	       
-	    	       $("#myModal3").on('hidden.bs.modal', function () {
-	    	           window.location.href = importpoint_jsp;
-	    	       });
-	        	}
+			if (data.message=="1")
+			{
+				$("#myModal3").modal({                    
+					"backdrop"  : "static",
+					"keyboard"  : true,
+					"show"      : true                     
+				});
+
+				$("#myModal3").on('hidden.bs.modal', function () {
+					window.location.href = importpoint_jsp;
+				});
+			}
 		},		error : function(xhr,status,errorthrown){
 			console.log(JSON.stringify(xhr + " " + status + " " + errorthrown));
 		}
@@ -352,34 +241,30 @@ function Delete(id)
 
 function Modifier(id_modif,old_name,old_radius)
 {
-	
-//	console.log(id);
-//	console.log(old_name);
-//	console.log(old_radius);
 
-    $("#myModal4").modal({                    
-	      "backdrop"  : "static",
-	      "keyboard"  : true,
-	      "show"      : true                     
-	    });
-  
+
+	$("#myModal4").modal({                    
+		"backdrop"  : "static",
+		"keyboard"  : true,
+		"show"      : true                     
+	});
+
 	$('#enreg').on(
 			'click',
 			function(evt)
 			{
-			    nom_modif = $("#myModal4").find("#nom_input").val();
-			    radius_modif = $("#myModal4").find("#radius_input").val();
+				nom_modif = $("#myModal4").find("#nom_input").val();
+				radius_modif = $("#myModal4").find("#radius_input").val();
 
-			    
-			    if (nom_modif.length==0) nom_modif = old_name;
-			    if (radius_modif.length==0) radius_modif = old_radius;
-//			    console.log(nom_modif);
-//			    console.log(radius_modif);
-			    
+
+				if (nom_modif.length==0) nom_modif = old_name;
+				if (radius_modif.length==0) radius_modif = old_radius;
+
+
 				updatePoint(id_modif,radius_modif,nom_modif); 
 
 			}
-			);
+	);
 }
 
 function updatePoint(id_modif,radius_modif,name_modif)
@@ -392,20 +277,20 @@ function updatePoint(id_modif,radius_modif,name_modif)
 		success : function (data)
 		{
 
-	        if (data.message=="1")
-	        	{
-	    	       $("#myModal4").hide(); 
+			if (data.message=="1")
+			{
+				$("#myModal4").hide(); 
 
-	    	       $("#myModal3").modal({                    
-	    		      "backdrop"  : "static",
-	    		      "keyboard"  : true,
-	    		      "show"      : true                     
-	    		    });
-	    	       
-	    	       $("#myModal3").on('hidden.bs.modal', function () {
-	    	           window.location.href = importpoint_jsp;
-	    	       });
-	        	}
+				$("#myModal3").modal({                    
+					"backdrop"  : "static",
+					"keyboard"  : true,
+					"show"      : true                     
+				});
+
+				$("#myModal3").on('hidden.bs.modal', function () {
+					window.location.href = importpoint_jsp;
+				});
+			}
 		},		error : function(xhr,status,errorthrown){
 			console.log(JSON.stringify(xhr + " " + status + " " + errorthrown));
 		}
@@ -417,18 +302,164 @@ function getMarkerDetail(lat,lon)
 {
 	$.ajax({
 		type : "GET",
-		url : GetPointMarkerDetails,
-	    async: false,
+		url : "GetPointMarkerDetail",
+		async: false,
 		data : "lat=" + lat + "&lon=" + lon,
 		dataType : "JSON",
 		success : function (data)
 		{
-	       id=data.id;
-           name =data.name;
-           radius=data.radius;
+			id=data.id;
+			name =data.name;
+			radius=data.radius;
 		},
 		error : function(xhr,status,errorthrown){
 			console.log(JSON.stringify(xhr + " " + status + " " + errorthrown));
 		}
 	});
+}
+
+
+/********************************** Afficher tous les points récupérés ************************************/
+
+function affiche() 
+{
+
+	var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
+
+	/********     Afficher les points de l'utilisateur **********/
+	for (var i=0; i< json1.expts.length; i++)
+	{
+
+		var lon = json1.expts[i].lon;
+		var lat = json1.expts[i].lat;
+		if(bool==0)
+			var feature1 = new OpenLayers.Feature.Vector
+			(
+
+					new OpenLayers.Geometry.Point( lon, lat ),
+					{description: "Mes amis sur ce lieu: "+json1.expts[i].name} ,
+					{externalGraphic: '/KasuKasu/data/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+			); 
+		if(bool==1)
+			feature1 = new OpenLayers.Feature.Vector
+			(
+
+					new OpenLayers.Geometry.Point( lon, lat ),
+					{description: "My friends in this place : "+json1.expts[i].name} ,
+					{externalGraphic: '/KasuKasu/data/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+			); 
+		vectorLayer.addFeatures(feature1);
+		table_feature1.push(feature1);
+
+	}
+
+	/********     Afficher les points des amis de l'utilisateur    **********/
+
+
+	for (var i=0; i< json2.expts.length; i++)
+	{
+
+		var lon = json2.expts[i].lon;
+		var lat = json2.expts[i].lat;
+		id = json2.expts[i].id;
+		if(bool==0)
+			var feature2 = new OpenLayers.Feature.Vector
+			(
+
+					new OpenLayers.Geometry.Point( lon, lat ),
+					{description: "Lieu : "+json2.expts[i].name+", "+json2.expts[i].radius+"m"},
+					{externalGraphic: '/KasuKasu/data/marker-red.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+			); 
+		if(bool==1)
+			feature2 = new OpenLayers.Feature.Vector
+			(
+
+					new OpenLayers.Geometry.Point( lon, lat ),
+					{description: "Place : "+json2.expts[i].name+", "+json2.expts[i].radius+"m"} ,
+					{externalGraphic: '/KasuKasu/data/marker-red.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+			); 
+		
+		vectorLayer.addFeatures(feature2);
+		table_feature2.push(feature2);
+
+
+	}
+
+	map.addLayer(vectorLayer);
+
+	var controls = {
+			selector: new OpenLayers.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
+	};
+
+	function createPopup(feature) 
+	{
+		/********     Premier modèle de pop up     **********/
+
+		if (containsObject(feature, table_feature1))
+		{
+			var lonLat = feature.geometry.getBounds().getCenterLonLat();
+
+			feature.popup = new OpenLayers.Popup.FramedCloud("pop",
+					feature.geometry.getBounds().getCenterLonLat(),
+					null, 	          
+					'<div class="markerContent">'+feature1.attributes.description+'</div>'+'<br></br>'+'<button onclick="abonnement(\'' + lonLat.lat + '\',\'' + lonLat.lon + '\')">S\'abonner</button>',
+					null,
+					true,
+					function() { controls['selector'].unselectAll(); }
+			);
+
+			map.addPopup(feature.popup);   
+
+		}
+
+		/********     Deuxième modèle de pop up     **********/
+
+		if (containsObject(feature, table_feature2))
+		{
+			var lonLat = feature.geometry.getBounds().getCenterLonLat();
+
+			id="";
+			name="";
+			radius="";
+
+			getMarkerDetail(lonLat.lat,lonLat.lon);
+
+
+			feature.popup = new OpenLayers.Popup.FramedCloud
+			(
+					"popup",
+					feature.geometry.getBounds().getCenterLonLat(),
+					null, 	          
+					'<div class="markerContent">'+feature.attributes.description+'</div>'+'<br></br>'+'<button onclick="Modifier(\'' + id + '\',\'' + name + '\' ,\'' + radius+ '\')"> Modifier</button>'+'<button onclick="Delete(\''+id+'\');">Supprimer</button>',
+					null,
+					true,
+					function() { controls['selector'].unselectAll(); }
+			);
+
+			map.addPopup(feature.popup);
+		}	         
+	}
+
+	function destroyPopup(feature) {
+
+		feature.popup.destroy();
+		feature.popup = null;
+
+	}
+
+	map.addControl(controls['selector']);
+	controls['selector'].activate();
+
+
+}
+
+function containsObject(obj, list) {
+	var i;
+	for (i = 0; i < list.length; i++) {
+		if (list[i] === obj) {
+			return true;
+		}
+	}
+
+	return false;
 }
