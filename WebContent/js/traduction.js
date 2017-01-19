823c847a42f360e873257b28b88b2b41a27365c4
A joan <tutanck@gmail.com>
2017-01-19 19:32:39 +0100

    1er version fonctionnelle de walkthrough

--- WebContent/js/traduction.js
+++ WebContent/js/traduction.js
@@ -25,35 +25,35 @@ function trans()
 	//get the file name
 	var href = document.location.href;
 	var fileName = href.substr(href.lastIndexOf('/') + 1);
-	
+
 	xhr.open("GET","/KasuKasu/traduction_.json",false);
 	xhr.send();
 	//xhr.onreadystatechange = function() {
 
 	/*if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) 
 	{*/
-		dico = JSON.parse(xhr.responseText);
-		//console.log(JSON.stringify(dico));
-		
-		
-		console.log(walk_through(dico,"portal.jsp","titre","fr","origin"));
-		
-			
+	dico = JSON.parse(xhr.responseText);
+	//console.log(JSON.stringify(dico));//debug
+
+	for(var i in dico){
+		var json= dico[i];
+		if(dico[i].hasOwnProperty("portal.jsp"))
+		console.log(walk_through(json,"portal.jsp","titre","fr"));
+	}
+
 	//}
-	
+
 }
 
 
-function walk_through(json,topField,nextField,langue,origin) {	
+function walk_through(json,topField,nextField,langue) {	
 	for (var key in json)
 		if (json.hasOwnProperty(topField)) 
-			return walk_through(json[topField],topField,nextField,langue,origin);
+			return walk_through(json[topField],topField,nextField,langue);
 		else if (json.hasOwnProperty(nextField)){ 
-			console.log(json[nextField][langue])
+			//console.log("dico['"+topField+"#"+nextField+"'] : "+json[nextField][langue])
 			return json[nextField][langue];
 		}
-		else 
-			return origin;
 }
 
 
